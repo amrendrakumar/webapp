53606f492c21f6287faff25475c4cab4e5cc2a1e
amrendrakumar <amrendra.kumarmca@gmail.com>
2024-07-14 14:31:29 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! checking POL scm" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Checking GIt HUB change!" );
     }
 }
